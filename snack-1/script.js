16fa4613eddec3685423e902c2e5e8bc0b2f10b5
manuelpezzi <pezzimanuel@gmail.com>
2025-01-27 11:58:47 +0100

    primo snack complete

---
+++ snack-1/script.js
@@ -0,0 +1,4 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+names.forEach((Element) => { console.log(Element) })
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
