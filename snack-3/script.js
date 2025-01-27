eda6962905969e85ec699ea38c685ba1a193a029
manuelpezzi <pezzimanuel@gmail.com>
2025-01-27 12:04:54 +0100

    snack3 complete

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,5 @@
 const numbers = [2, 8, 4, 7, 2, 87];
-
+const numbersPlausOne = numbers.map((number) => number + 1);
+console.log(numbersPlausOne)
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
