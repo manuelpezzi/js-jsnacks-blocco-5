c9d4d19e4e5a20833d93fb1417f3cfcdac4bcb43
manuelpezzi <pezzimanuel@gmail.com>
2025-01-27 12:10:21 +0100

    snack5 complete

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,6 @@
 const nums = [2, 8, 4, 7, 12, 87];
+const eventNumbers = nums.filter((num) => num % 2 === 0)
+console.log(eventNumbers)
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
